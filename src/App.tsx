4ff7d332714337b0c88255de00b6f9c40a51a47d
Turing-jiang <gg.jiang87@gmail.com>
2024-08-12 17:50:17 +0900

    refactor:update test file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from '@components/Button'
 import { isDarkMode, toggleDarkMode, setLightMode, setDarkMode } from '@/lib/utils'
+import { Button as MyButton } from '@components/button/Button'
+import GeneralButton from '@components/button/GeneralButton'
+import FilterButton from '@components/button/FilterButton'
+import TabButton from '@components/button/TabButton'
 
 function App() {
   const [theme, setTheme] = useState<string>(isDarkMode().toString())
+
+  useEffect(() => {
+    handleSetDarkMode()
+  }, [])
 
   function handleToggleDarkMode() {
     toggleDarkMode()
@@ -22,13 +30,18 @@ function App() {
 
   return (
     <>
-      <p>{theme}</p>
-      <div className="flex gap-x-[15px]">
+      <div className="flex gap-x-[15px] text-black dark:text-white">
+        <p>{theme}</p>
         <button onClick={handleToggleDarkMode}>toggle</button>
         <button onClick={handleSetDarkMode}>setDark</button>
         <button onClick={handleSetLightMode}>setLight</button>
       </div>
       <Button></Button>
+      <MyButton className="px-[15px] py-[3px]">닫기</MyButton>
+      <GeneralButton>닫기</GeneralButton>
+      <FilterButton></FilterButton>
+      <TabButton shape="hug">Text</TabButton>
+      <TabButton shape="fixed">Text</TabButton>
     </>
   )
 }
